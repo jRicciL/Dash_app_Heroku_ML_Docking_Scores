4d83b07ab719bffb3c704e7997a7973c639051ba
Joel <ricci.lob@gmail.com>
2020-07-15 20:37:31 -0700

    Layout updated

--- app.py
+++ app.py
@@ -9,25 +9,21 @@ import pandas as pd
 from data_source import *
 
 
-app = dash.Dash(external_stylesheets=[dbc.themes.SANDSTONE])
+app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
+    "background-color": "#F2EFE4",
+    "margin-right": "1rem",
+    "border-color": "#73726B",
+    "border-radius": '0'
 }
 
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
+    "margin-left": "0",
+    "margin-right": "1rem"
 }
 
 
@@ -36,26 +32,74 @@ split = 'rand'
 selector = 'LR'
 fig = line_plot_metrics(split, selector, metric)
 
-sidebar = html.Div([
-        html.H2("Sidebar", className="display-4"),
+controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("Train-Test split method:", className='font-weight-bold'),
+                dbc.RadioItems(
+                    id="split-value",
+                    options=[
+                        {'label': value, 'value': key} for key, value in split_names.items()
+                    ],
+                    value='rand',
+                ),
+            ],
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Conformational Selection method:", className='font-weight-bold'),
+                dbc.RadioItems(
+                    id="selector-value",
+                    options=[
+                        {'label': value, 'value': key} for key, value in selector_names.items()
+                    ],
+                    value='rand',
+                ),
+            ],
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Evaluation Metric:", className='font-weight-bold'),
+                dcc.Dropdown(
+                    id="metric-value",
+                    options=[
+                        {'label': value, 'value': key} for key, value in metric_names.items()
+                    ],
+                    value="roc_auc",
+                ),
+            ]
+        )
     ],
-    style=SIDEBAR_STYLE,
+    body=True,
+    style=SIDEBAR_STYLE
 )
 
-content = html.Div(
-    [
+line_plot = [
+        html.H5('Título', className='text-center'),
         dcc.Graph(
-            id='basic-interactions',
-            figure=fig,
-            config=plotly_conf
+                id='basic-interactions',
+                figure=fig,
+                config=plotly_conf
+            )
+    ]
+
+
+app.layout = dbc.Container(
+    [
+        html.H1("CDK2 & FXa Results"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(controls, md=3),
+                dbc.Col(line_plot, md=9),
+            ],
+            align="center",
+            style=CONTENT_STYLE
         ),
     ],
-    id="page-content",
-    style=CONTENT_STYLE,
+    fluid=True,
 )
-
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
-
 
 
 if __name__ == '__main__':
